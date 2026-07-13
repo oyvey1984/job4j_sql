a346d904d6428da791ab06e9c35c2fac44be843c
Oy vey <oyiveyi@gmail.com>
2026-07-13 20:28:08 +0300

    2.3.5.1. SELECT [#505606 #565564]

--- scripts/505606_select/001_dml_select.sql
+++ scripts/505606_select/001_dml_select.sql
@@ -1,19 +1,20 @@
-select * from users u ;
+SELECT * FROM users u;
 
-select u.name, u.email  from users u ;
+SELECT u.name, u.email FROM users u;
 
-select u.id user_id, u.name user_name, u.email user_email from users u;
+SELECT u.id user_id, u.name user_name, u.email user_email FROM users u;
 
-select
-oi.id order_id,
-oi.product_id,
-oi.quantity,
-oi.unit_price,
-oi.quantity * oi.unit_price line_total from order_items oi;
+SELECT
+    oi.id order_id,
+    oi.product_id,
+    oi.quantity,
+    oi.unit_price,
+    oi.quantity * oi.unit_price line_total
+FROM order_items oi;
 
-select
-id,
-name,
-price,
-price * 0.9 discounted_price
-from products p;
+SELECT
+    id,
+    name,
+    price,
+    price * 0.9 discounted_price
+FROM products p;
