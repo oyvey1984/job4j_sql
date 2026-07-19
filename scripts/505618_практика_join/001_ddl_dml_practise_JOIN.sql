dfb977995cefcabe4d1ea43a3524e994c3e670d2
Oy vey <oyiveyi@gmail.com>
2026-07-19 21:53:24 +0300

    2.3.6.6. Практика JOIN [#505618]

--- scripts/505618_практика_join/001_ddl_dml_practise_JOIN.sql
+++ scripts/505618_практика_join/001_ddl_dml_practise_JOIN.sql
@@ -88,3 +88,139 @@ VALUES
     ('Concept Car B', 4, NULL, 7),
     ('Prototype X', NULL, NULL, NULL),
     ('Old Van', 8, 5, NULL);
+
+SELECT
+	cb.id car_bodies_id,
+	cb."name" car_bodies_name
+FROM car_bodies cb
+LEFT JOIN cars c ON cb.id = c.body_id
+WHERE c.body_id IS NULL;
+
+SELECT
+	ce.id car_engines_id,
+	ce."name" car_engines_name
+FROM car_engines ce
+LEFT JOIN cars c ON ce.id = c.engine_id
+WHERE c.engine_id IS NULL;
+
+SELECT
+	ct.id car_transmissions_id,
+	ct."name" car_transmissions_name
+FROM car_transmissions ct
+LEFT JOIN cars c ON ct.id = c.transmission_id
+WHERE c.transmission_id IS NULL;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name
+FROM cars c
+LEFT JOIN car_bodies cb ON c.body_id = cb.id;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name,
+	ce."name" engine_name,
+	ct."name" transmission_name
+FROM cars c
+INNER JOIN car_bodies cb ON c.body_id = cb.id
+INNER JOIN car_engines ce ON c.engine_id = ce.id
+INNER JOIN car_transmissions ct ON c.transmission_id = ct.id ;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name,
+	ce."name" engine_name
+FROM cars c
+INNER JOIN car_engines ce ON c.engine_id = ce.id
+LEFT JOIN car_bodies cb ON c.body_id = cb.id
+WHERE c.body_id IS NULL;
+
+SELECT
+	cb.id body_id,
+	cb."name" body_name,
+	c.id car_id,
+	c."name" car_name
+FROM car_bodies cb
+LEFT JOIN cars c ON cb.id = c.body_id ;
+
+SELECT
+	ce.id,
+	ce."name" engine_name
+FROM car_engines ce
+LEFT JOIN cars c ON ce.id = c.engine_id
+WHERE c.engine_id IS NULL;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name,
+	ce."name" engine_name,
+	ct."name" transmission_name
+FROM cars c
+LEFT JOIN car_bodies cb ON c.body_id = cb.id
+LEFT JOIN car_engines ce ON c.engine_id = ce.id
+LEFT JOIN car_transmissions ct ON c.transmission_id = ct.id
+WHERE ct."name" ILIKE 'automatic%';
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name,
+	ce."name" engine_name,
+	ct."name" transmission_name
+FROM cars c
+LEFT JOIN car_bodies cb ON c.body_id = cb.id
+LEFT JOIN car_engines ce ON c.engine_id = ce.id
+LEFT JOIN car_transmissions ct ON c.transmission_id = ct.id
+WHERE
+	cb."name" IS NULL
+	OR ce."name" IS NULL
+	OR ct."name" IS NULL;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	ce."name" engine_name,
+	ct."name" transmission_name
+FROM cars c
+INNER JOIN car_engines ce ON c.engine_id = ce.id
+LEFT JOIN car_transmissions ct ON c.transmission_id = ct.id;
+
+SELECT
+	'body' AS detail_type,
+	cb.id detail_id,
+	cb."name" detail_name
+FROM car_bodies cb
+LEFT JOIN cars c ON cb.id = c.body_id
+WHERE c.body_id  IS NULL
+UNION ALL
+SELECT
+	'engine' AS detail_type,
+	ce.id detail_id,
+	ce."name" detail_name
+FROM car_engines ce
+LEFT JOIN cars c ON ce.id = c.engine_id
+WHERE c.engine_id IS NULL
+UNION ALL
+SELECT
+	'transmission' AS detail_type,
+	ct.id detail_id,
+	ct."name" detail_name
+FROM car_transmissions ct
+LEFT JOIN cars c ON ct.id = c.transmission_id
+WHERE c.transmission_id IS NULL;
+
+SELECT
+	c.id,
+	c."name" car_name,
+	cb."name" body_name,
+	ce."name" engine_name,
+	ct."name" transmission_name
+FROM cars c
+LEFT JOIN car_bodies cb ON c.body_id = cb.id
+LEFT JOIN car_engines ce ON c.engine_id = ce.id
+LEFT JOIN car_transmissions ct ON c.transmission_id = ct.id
+WHERE cb."name" ~* '(sedan|hatchback|suv)';
