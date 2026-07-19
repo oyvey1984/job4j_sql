8783857949331edde66daebac2d7f750c69516aa
Oy vey <oyiveyi@gmail.com>
2026-07-19 16:54:23 +0300

    2.3.6.3. LEFT, RIGHT, FULL JOIN и др. [#505615]

--- scripts/505615_left_right_full_join/001_ddl_dml_LEFT_RIGHT_FULL_JOIN.sql
+++ scripts/505615_left_right_full_join/001_ddl_dml_LEFT_RIGHT_FULL_JOIN.sql
@@ -63,3 +63,66 @@ VALUES
     ('Ноутбуки', 1),
     ('Аксессуары', 1),
     ('Чехлы', 4);
+
+
+SELECT
+	u.id user_id,
+	u.name user_name,
+	count(o.id) order_count
+FROM users u
+LEFT JOIN orders o ON u.id = o.user_id
+GROUP BY u.id
+ORDER BY order_count DESC;
+
+SELECT
+	o.id,
+	o.status,
+	o.created_at
+FROM orders o
+LEFT JOIN payments p ON o.id = p.order_id
+WHERE p.order_id IS NULL;
+
+SELECT
+    p.id product_id,
+    p.name product_name,
+    COUNT(oi.id) order_count
+FROM products p
+LEFT JOIN order_items oi ON p.id = oi.product_id
+GROUP BY p.id, p.name
+ORDER BY order_count DESC;
+
+SELECT
+	r.id role_id,
+	r.name role_name,
+	count(ur.user_id)
+FROM roles r
+LEFT JOIN user_roles ur ON r.id = ur.role_id
+GROUP BY r.id
+ORDER BY role_id;
+
+SELECT
+	u.id,
+	u."name",
+	ur.role_id
+FROM users u
+LEFT JOIN user_roles ur ON u.id = ur.user_id
+WHERE ur.role_id IS NULL;
+
+SELECT
+	r.id role_id,
+	ur.user_id user_id
+FROM roles r
+FULL JOIN user_roles ur ON r.id = ur.role_id;
+
+SELECT
+	r.code,
+	e.code
+FROM roles r
+CROSS JOIN environments e;
+
+SELECT
+	c.id,
+	c."name" category_name,
+	c2."name" parent_category_name
+FROM categories c
+LEFT JOIN categories c2 ON c.parent_id = c2.id
